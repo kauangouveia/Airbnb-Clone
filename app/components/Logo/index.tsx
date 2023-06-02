d023a3b7f0f02a8da160c08d83516b3c370420c5
kauangouveia <kauangouveia64@gmail.com>
2023-06-02 11:27:14 -0300

    chore: create component logo

---
+++ app/components/Logo/index.tsx
@@ -0,0 +1,9 @@
+'use-client'
+import Image from "next/image"
+import { useRouter } from "next/router"
+
+export default function Logo() {
+    return (
+        <Image alt='logo' className="hidden md:block cursor-pointer" height='100' width='100' src='/images/logo.png'/>
+    )
+}
