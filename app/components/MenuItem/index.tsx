d531579bb5f384782d65e0b25d5430d7991c5923
kauangouveia <kauangouveia64@gmail.com>
2023-06-02 12:11:04 -0300

    chore: create component menu options

---
+++ app/components/MenuItem/index.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+interface MenuItemProps {
+    onClick: () => void;
+    label: string;
+}
+
+export function MenuItem({ label, onClick }: MenuItemProps) {
+    return (
+        <div className="px-4 py-3 hover:bg-neutral-100 transition font-semibold" onClick={onClick}>{label}</div>
+    )
+}
