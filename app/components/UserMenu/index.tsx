d34017702427aa8b39e6afe117cb0bc898d97119
kauangouveia <kauangouveia64@gmail.com>
2023-06-02 12:10:49 -0300

    chore: create options for menu

--- app/components/UserMenu/index.tsx
+++ app/components/UserMenu/index.tsx
@@ -1,15 +1,23 @@
 'use client';
 import { AiOutlineMenu } from 'react-icons/ai'
 import { Avatar } from '../Avatar';
+import { useCallback, useState } from 'react';
+import { MenuItem } from '../MenuItem';
 
 export function UserMenu() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleOpen = useCallback(() => {
+        setIsOpen((value) => !value);
+    }, [])
+
     return (
         <div className="relative">
             <div className="flex flex-row items-center gap-3">
                 <div onClick={() => { }} className="hidden md:block text-sm font-semibold  py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer" >
                     Airbnb your home
                 </div>
-                <div onClick={() => { }} className="
+                <div onClick={toggleOpen} className="
                   p-4
                   md:py-1
                   md:px-2
@@ -30,6 +38,16 @@ export function UserMenu() {
                     </div>
                 </div>
             </div>
+            {isOpen && (
+                <div className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm '>
+                    <div className='flex flex-col cursor-pointer'>
+                        <>
+                            <MenuItem label='Login' onClick={() => { }} />
+                            <MenuItem label='Sign up' onClick={() => { }} />
+                        </>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
